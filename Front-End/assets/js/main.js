62b8f5ffc23fcc975e5f1d838bc5ef84b759bc8e
MoamenZyan <moamenzyan20@gmail.com>
2024-03-12 23:37:19 +0200

    signin page

--- Front-End/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-const togglePassword = document.getElementById('toggler');
-const password = document.querySelector('#fakePassword');
-
-togglePassword.addEventListener('click', function () {
-  password.type = password.type === 'password' ? 'text' : 'password';
-  this.classList.toggle('fa-eye');
-  this.classList.toggle('fa-eye-slash');
-});
-
